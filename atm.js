39fdad7653d1e645b89d0191403d10ec3ffa8423
Hp <qnyazface@maul.ru>
2023-06-16 17:12:34 +0400

    alert added

--- atm.js
+++ atm.js
@@ -4,19 +4,19 @@ let pin = 123;
 function withdraw(amount) {
     if (balance >= amount) {
         balance = balance - amount;
-        console.log(`Emeliyyat ugurla heyata kecirildi ${amount} Azn hesabdan coxarildi hesab ${balance}`);
+        alert(`Emeliyyat ugurla heyata kecirildi ${amount} Azn hesabdan coxarildi hesab ${balance}`);
     } else {
-        console.log("Hesabda yeterince mebleg (AZN) yoxdur");
+        alert("Hesabda yeterince mebleg (AZN) yoxdur");
     }
 }
 
 function toPutIn(amount) {
     balance = balance + amount;
-    console.log(`Emelliyyat ugurla heyat kecirildi ${amount} AZN hesaba elave edildi hesab ${balance} AZN`);
+    alert(`Emelliyyat ugurla heyat kecirildi ${amount} AZN hesaba elave edildi hesab ${balance} AZN`);
 }
 
 function viewBalance() {
-    console.log(`Sizin balansiniz ${balance} AZN`);
+    alert(`Sizin balansiniz ${balance} AZN`);
 }
 
 function resetPin() {
@@ -24,29 +24,29 @@ function resetPin() {
     if (pin === oldpin) {
         let newPin = Number(prompt("Yeni PIN kodunuzu yazin: "));
         pin = newPin;
-        console.log(`Pin kodunuz ugurla yenilendi yeni pin kodunuz: ${pin}`);
+        alert(`Pin kodunuz ugurla yenilendi yeni pin kodunuz: ${pin}`);
     } else {
-        console.log("Pin yalnis daxil edilib");
+        alert("Pin yalnis daxil edilib");
     }
 }
 
 while (true) {
- let option = prompt("ATM e xos geldiniz. Funksiyani secin (1 - Balansin yoxlanisi, 2 - Hesabdan cixaris, 3 - Hesaba Medaxil, 4 - PIN-i deyisdirmek, 5 - Cixis):");
+    let option = prompt("ATM e xos geldiniz. Funksiyani secin (1 - Balansin yoxlanisi, 2 - Hesabdan cixaris, 3 - Hesaba Medaxil, 4 - PIN-i deyisdirmek, 5 - Cixis):");
 
-  if (option === "1") {
-    viewBalance();
-  } else if (option === "2") {
-    let amount = Number(prompt("Cixartmaq istediyiniz miqdari girin: "));
-    withdraw(amount);
-  } else if (option === "3") {
-    let amount = Number(prompt("Elave etmek isdediyiniz miqdari girin: "));
-    toPutIn(amount);
-  } else if (option === "4") {
-    resetPin();
-  } else if (option === "5") {
-    console.log("ATM-dan cixildi.");
-    break;
-  } else {
-    console.log("Duzgun reqem secin");
-  }
+    if (option === "1") {
+        viewBalance();
+    } else if (option === "2") {
+        let amount = Number(prompt("Cixartmaq istediyiniz miqdari girin: "));
+        withdraw(amount);
+    } else if (option === "3") {
+        let amount = Number(prompt("Elave etmek isdediyiniz miqdari girin: "));
+        toPutIn(amount);
+    } else if (option === "4") {
+        resetPin();
+    } else if (option === "5") {
+        alert("ATM-dan cixildi.");
+        break;
+    } else {
+        alert("Duzgun reqem secin");
+    }
 }
